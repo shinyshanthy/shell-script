f6b86a7ab2b253bfd0cd59af0eaca2087fffafbf
Prasanthi <shinyshanthy1407@gmail.com>
2023-08-18 14:46:23 +0530

    script added

--- 01variables.sh
+++ 01variables.sh
@@ -4,7 +4,7 @@ echo "Hello world , iam learning DevOps"
 PERSON1=$1 #this is first comand line
 PERSON2=$2 #this is second comand line
 
-echo "$PERSON1 : good morning suresh"
+echo "$PERSON1 : good morning $PERSON2"
 echo "Good morning $PERSON1"
 echo "How aer you $PERSON2"
 echo "how are you $PERSON1"
