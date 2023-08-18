735912cb2860b8253758987f082ab559c67edd87
Prasanthi <shinyshanthy1407@gmail.com>
2023-08-18 15:36:49 +0530

    condition

--- install-mysql.sh
+++ install-mysql.sh
@@ -9,3 +9,22 @@ then
 fi
 
 yum install mysql -y 
+
+if [ $? -no 0 ]
+then
+    echo "installation of mysql is error"
+    exit 1
+else
+    echo "installation of mysql is success"
+fi
+
+
+yum install postfix -y
+
+if [ $? -no 0 ]
+then 
+    echo "installation of postfix is error"
+    exit 1
+else
+    echo "installation of postfix is success"
+fi
