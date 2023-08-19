c84b721d297cdedbbf64fedb9e2e8da230705f3e
Prasanthi <shinyshanthy1407@gmail.com>
2023-08-19 09:22:17 +0530

    dellog

---
+++ dellog.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+APP_LOGS_DIR=/home/centos/app-logs
+ DATE=$(DATE +%F:%H:%M:%S)
+ LOGDIR=/home/centos/shell-script-logs
+
+ SCRIPT_NAME=$0
+ LOGFILE=$LOGDIR/$0-$DATE.log
+
+ FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -ntime +14)
+
+ echo "script started executing at $DATE"
+
+ while read line
+ do     
+    echo "Deleting $line" &>> $LOGFILE
+    rm -rf $line
+done <<< $FILES_TO_DELETE
