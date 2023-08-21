c927b42b43b9e508f925be1cd5826aa7ac433759
Prasanthi <shinyshanthy1407@gmail.com>
2023-08-21 15:19:43 +0530

    disk script

---
+++ disk_mgnt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+LOGFILE_DIRECTORY=/tmp
+DATE=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOGFILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME=$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+DISK_USUAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
+DISK_USUAGE_THRESHOLD=1
+
+while IFS= read line
+do 
+    echo "output: $line"
+done <<< $DISK_USAGE
